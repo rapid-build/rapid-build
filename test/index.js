92c11cbd3a55382652a02abf6ab3da955696ce8b
Judd Younce <jyounce@yahoo.com>
2016-02-22 03:10:59 -0600

    test(ci): run tests with ci

--- test/index.js
+++ test/index.js
@@ -2,7 +2,6 @@
  * Get Config
  * Run Tests
  ******************/
-return
 require('coffee-script/register');
 var config = require('../core/config')();
 require(config.paths.abs.test.init)(config);
