d21bf4005389ff68fcdf9e79b12ced708a268d1d
Judd Younce <jyounce@yahoo.com>
2016-03-22 15:59:46 -0500

    docs(test) doc test api

--- test/index.js
+++ test/index.js
@@ -1,6 +1,15 @@
-/* Register Coffee
- * Run Tests
- ******************/
+/* TEST API (can combine args too)
+ * npm test
+ * npm test watch
+ * npm test prod:server
+ * npm test options:*
+ * npm test options:exclude
+ * npm test options:build:exclude
+ * npm test verbose
+ * npm test verbose:*
+ * npm test verbose:tasks
+ * npm test verbose:jasmine
+ **********************************/
 require('coffee-script/register');
 var path     = require('path');
 var initPath = path.join(__dirname, 'init', 'index');
