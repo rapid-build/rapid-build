0deac7ce0c609e4b43031aad56e532dd9b57a8b5
Judd Younce <jyounce@yahoo.com>
2016-04-19 23:27:24 -0500

    docs(deploy scripts): add deploy master and latest tag script

--- docs/scripts/build-prod-check/index.js
+++ docs/scripts/build-prod-check/index.js
@@ -1,10 +1,11 @@
+require('coffee-script/register');
+
 var path   = require('path'),
 	rbRoot = path.resolve(__dirname, '..', '..', '..'),
 	msg    = 'Do you need to build prod before you commit?',
 	skip   = 'Skip this by adding --no-verify.',
-	sep    = ('-').repeat(msg.length);
+	sep    = '-'.repeat(msg.length);
 
-require('coffee-script/register');
 require(`${rbRoot}/extra/tasks/add-colors`)();
 
 /* Message
---
+++ docs/scripts/deploy/index.js
@@ -0,0 +1,58 @@
+require('coffee-script/register');
+
+var path     = require('path'),
+	rbRoot   = path.resolve(__dirname, '..', '..', '..'),
+	pkgPath  = path.resolve(__dirname, '..', '..', 'package.json'),
+	pkg      = require(pkgPath),
+	version  = pkg.version,
+	location = process.argv.slice(2)[0], // master or latest tag
+	location = location == 'tag' ? `v${version}` : location,
+	deploy   = require('./deploy'),
+	needle   = 'unexpected error', // random gandi error at beginning of deploy
+	cnt      = 1,
+	log,
+	runDeploy;
+
+/* Helpers
+ **********/
+require(`${rbRoot}/extra/tasks/add-colors`)();
+
+log = (msg, type) => {
+	type = type || 'attn';
+	var sep    = '-'.repeat(msg.length),
+		method = type == 'attn' ? 'log' : type;
+	console[method](sep[type]);
+	console[method](msg[type]);
+	console[method](sep[type]);
+};
+
+/* Deploy Docs
+ **************/
+runDeploy = () => {
+	var msg = '';
+	return deploy(location).then(result => {
+		if (cnt == 5) {
+			msg = `Docs Deployment Failed from ${location}. ${needle}`;
+			return log(msg, 'error');
+		} else if (result.indexOf('Aborting deployment') != -1) {
+			console.log(result);
+			msg = `Docs Deployment Failed from ${location}`;
+			return log(msg, 'error');
+		} else if (result.indexOf(needle) != -1) {
+			cnt++;
+			log(`Redeploy CNT ${cnt}`);
+			console.log(result);
+			return runDeploy();
+		}
+		console.log(result);
+		log(`Docs Deployed from ${location}`);
+	}).catch(e => {
+		msg = `Docs Deployment Failed from ${location}`;
+		log(msg, 'error');
+		console.error(`Error: ${e.message}`.error);
+	});
+};
+
+/* RUN IT!
+ **********/
+runDeploy();
