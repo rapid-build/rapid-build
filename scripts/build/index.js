bd64e0961e376cd66c1094b22a0c921ff1a96a1a
jyounce <jyounce@yahoo.com>
2016-09-19 15:56:58 -0500

    chore(build): move tsconfig.json and typings.json inside scripts/build

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -1,5 +1,10 @@
 /* BUILD ENTRY
  * Excluded from watch.
  ***********************/
-require('ts-node/register')
+var path     = require('path'),
+	tsconfig = path.join(__dirname, 'tsconfig.json');
+
+require('ts-node').register({
+	project: tsconfig
+})
 require('./runBuild')()
